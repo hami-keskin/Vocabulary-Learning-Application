0992b37e969c984d2177b4f8e810db97c770e295
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-11-30 19:00:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from utils import show_notification  # show_notification fonksiyonunu içeri aktar
 
 from file_operations import load_words, save_words
 from gui_components import apply_dark_mode, clear_window, center_frame, create_button
@@ -15,15 +15,18 @@ def main_menu(root):
 
     def exit_program():
         save_words(file_path, words)
-        if messagebox.askyesno("Çıkış", "Programdan çıkmak istediğinizden emin misiniz?"):
-            root.destroy()
+        root.after(100, root.quit)  # 5 saniye sonra programı kapat
+
+    def save_and_notify():
+        save_words(file_path, words)  # Save the words to file
+        show_notification(root, "Veriler başarıyla kaydedildi!")  # Show notification after save
 
     clear_window(root)
     frame = center_frame(root)
     create_button(frame, "Yeni Kelime Öğren", lambda: learn_new_words_gui(root, words, file_path, main_menu))
     create_button(frame, "Kelime Tekrarı Yap", lambda: review_words_gui(root, words, file_path, main_menu))
     create_button(frame, "İstatistikleri Göster", lambda: show_statistics_gui(root, words, file_path, main_menu))
-    create_button(frame, "Kaydet", lambda: save_words(file_path, words))
+    create_button(frame, "Kaydet", save_and_notify)  # Save and show notification when clicked
     create_button(frame, "Çık", exit_program)
 
 root = tk.Tk()
