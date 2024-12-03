fa936a231822f573892a6bb2342d0f08bc699854
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-12-03 13:10:12 +0300

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,18 +2,21 @@ from gui_components import clear_window, create_label, create_button, center_fra
 
 def show_statistics_gui(root, words, file_path, main_menu):
     total = len(words)
-    known = sum(1 for data in words.values() if data.get("known", False))
-    memorized = sum(1 for data in words.values() if data["memorized"] and not data.get("known", False))
-    retry = sum(1 for data in words.values() if data["retry"])
-    unmemorized = total - memorized - known - retry
+    if total == 0:
+        stats = "Hiç kelime yok!"
+    else:
+        known = sum(1 for data in words.values() if data.get("known", False))
+        memorized = sum(1 for data in words.values() if data["memorized"] and not data.get("known", False))
+        retry = sum(1 for data in words.values() if data["retry"])
+        unmemorized = total - memorized - known - retry
 
-    stats = (
-        f"Toplam Kelime: {total}\n"
-        f"Bilinen Kelimeler: {known} ({known / total:.1%})\n"
-        f"Ezberlenen Kelimeler: {memorized} ({memorized / total:.1%})\n"
-        f"Tekrar Edilecek Kelimeler: {retry}\n"
-        f"Öğrenilmeyi Bekleyen Kelimeler: {unmemorized}\n"
-    )
+        stats = (
+            f"Toplam Kelime: {total}\n\n"
+            f"Bilinen Kelimeler: {known} ({known / total:.1%})\n"
+            f"Ezberlenen Kelimeler: {memorized} ({memorized / total:.1%})\n"
+            f"Tekrar Edilecek Kelimeler: {retry} ({retry / total:.1%})\n"
+            f"Öğrenilmeyi Bekleyen Kelimeler: {unmemorized} ({unmemorized / total:.1%})\n"
+        )
 
     clear_window(root)
     frame = center_frame(root)
